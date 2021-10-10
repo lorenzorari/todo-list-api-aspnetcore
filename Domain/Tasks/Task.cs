875584cfd0604c79234fb84368bb243ecbb33718
Lorenzo <lorenzofg24@gmail.com>
2021-10-10 23:51:16 +0200

    Task class created

---
+++ Domain/Tasks/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Tasks
+{
+    class Task : ITask
+    {
+        public string title { get; set; }
+        public bool isDone { get; set; }
+        public int Id { get; set; }
+
+        public Task() { }
+    }
+}
