7e768bff57eac04705f4f3045a3dddb89e838ef4
Lorenzo <lorenzofg24@gmail.com>
2021-10-10 23:46:36 +0200

    Implementing IEntity

--- Domain/Tasks/ITask.cs
+++ Domain/Tasks/ITask.cs
@@ -1,6 +1,8 @@
-﻿namespace Domain.Tasks
+﻿using Domain.Shared;
+
+namespace Domain.Tasks
 {
-    interface ITask
+    interface ITask : IEntity
     {
         string title { get; set; }
         bool isDone { get; set; }
