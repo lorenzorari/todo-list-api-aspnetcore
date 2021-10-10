b19109f9efb96c041c09f95f63409d63907e7145
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 00:44:01 +0200

    ITaskFactory created

---
+++ Infrastructure/SqlServer/ITaskFactory.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Tasks;
+using System.Data.SqlClient;
+
+namespace Infrastructure.SqlServer
+{
+    interface ITaskFactory
+    {
+        ITask CreateFromReader(SqlDataReader reader);
+    }
+}
