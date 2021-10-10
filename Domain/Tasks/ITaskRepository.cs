03f053b8907ff6fb2ba40826e38a83184a2212de
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 00:01:02 +0200

    ITaskRepository created

---
+++ Domain/Tasks/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Tasks
+{
+    interface ITaskRepository
+    {
+        IEnumerable<ITask> Query();
+        ITask Create(ITask task);
+        bool Update(int id, ITask task);
+        bool Delete(int id, ITask task);
+    }
+}
