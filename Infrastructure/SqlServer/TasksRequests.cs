bd8453399d2c13032368663581ef28b096066f8d
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 13:06:55 +0200

    Query & create requests created

---
+++ Infrastructure/SqlServer/TasksRequests.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.SqlServer
+{
+    public class TasksRequests
+    {
+        public static readonly string TableName = "tasks";
+        public static readonly string ColumnId = "id";
+        public static readonly string ColumnTitle = "title";
+        public static readonly string ColumnIsDone = "is_done";
+
+        public static readonly string ReqQuery = $"SELECT * FROM {TableName}";
+
+        public static readonly string ReqCreate = $@"
+            INSERT INTO {TableName} ({ColumnTitle}, {ColumnIsDone})
+            OUTPUT INSERTED.{ColumnId}
+            VALUES (@{ColumnTitle}, @{ColumnIsDone})
+        ";
+    }
+}
