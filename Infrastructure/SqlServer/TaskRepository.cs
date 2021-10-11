02a779f0cb705a7a62561e6f248b3e2a5cc9e3df
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 14:01:53 +0200

    GetTasks() implemented in TaskRepository

--- Infrastructure/SqlServer/TaskRepository.cs
+++ Infrastructure/SqlServer/TaskRepository.cs
@@ -1,13 +1,32 @@
 ﻿using Domain.Tasks;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Infrastructure.SqlServer
 {
     public class TaskRepository : ITaskRepository
     {
+        private ITaskFactory _taskFactory = new TaskFactory();
+
         public IEnumerable<ITask> GetTasks()
         {
-            throw new System.NotImplementedException();
+            IList<ITask> tasks = new List<ITask>();
+            
+            using (var sqlConnection = Database.GetConnection())
+            {
+                sqlConnection.Open();
+                var command = sqlConnection.CreateCommand();
+                command.CommandText = TasksRequests.ReqGetTasks;
+
+                var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (reader.Read())
+                {
+                    tasks.Add(_taskFactory.CreateFromReader(reader));
+                }
+            }
+
+            return tasks;
         }
 
         public ITask Create(ITask task)
