8a44c87f71a2967f6da996fd0d93cc0b7f779a54
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 12:52:41 +0200

    TaskFactory created

---
+++ Infrastructure/SqlServer/TaskFactory.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Tasks;
+using System.Data.SqlClient;
+
+namespace Infrastructure.SqlServer
+{
+    class TaskFactory : ITaskFactory
+    {
+        public ITask CreateFromReader(SqlDataReader reader)
+        {
+            return new Task
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                Title = reader.GetString(reader.GetOrdinal("title")),
+                IsDone = reader.GetBoolean(reader.GetOrdinal("is_done"))
+            };
+        }
+    }
+}
