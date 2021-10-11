49c1047541c612aed9fe2af27d9562dcf07f6b4a
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 13:37:22 +0200

    TaskRepository created

---
+++ Infrastructure/SqlServer/TaskRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Tasks;
+using System.Collections.Generic;
+
+namespace Infrastructure.SqlServer
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public IEnumerable<ITask> GetTasks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ITask Create(ITask task)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id, ITask task)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(int id, ITask task)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
