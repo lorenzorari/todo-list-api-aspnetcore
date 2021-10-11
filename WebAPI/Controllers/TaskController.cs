37ec29ed492aad1b6cebc5522ea48cd99716ec35
Lorenzo <lorenzofg24@gmail.com>
2021-10-11 21:35:04 +0200

    GetTasks controller created

---
+++ WebAPI/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Tasks;
+using Infrastructure.SqlServer;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskRepository _taskRepository = new TaskRepository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ITask>> GetTasks()
+        {
+            return Ok(_taskRepository.GetTasks());
+        }
+    }
+}
